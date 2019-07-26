848985170c2004b3194daeba308c6a2e60e88c09
caiogamedesign <caio.gamedesign@gmail.com>
2019-07-26 14:48:54 -0300

    Player rotation and tuning up the values

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,29 +1,58 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 
-public class Player : MonoBehaviour {
-
+public class Player : MonoBehaviour
+{
     [Tooltip("In ms^-1")] [SerializeField] float speed = 20f;
     [Tooltip("In m")] [SerializeField] float xRange = 5f;
     [Tooltip("In m")] [SerializeField] float yRange = 3f;
-    
+
+    [SerializeField] float positionPitchFactor = -5f;
+    [SerializeField] float controlThrowPitchFactor = -30f;
+
+    [SerializeField] float positionYawFactor = 4f;
+    [SerializeField] float controlThrowRollFactor = -20f;
+
+    private float xThrow, yThrow;
 
     private void Update()
     {
-        float clampedXPos = ClampedPos(transform.localPosition.x, "Horizontal", speed, xRange);
-        float clampedYPos = ClampedPos(transform.localPosition.y, "Vertical", speed, yRange);
+        ProcessPosition();
+        ProcessRotation();
+    }
+
+    private void ProcessRotation()
+    {
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+        float pitchDueToControlThrow = yThrow * controlThrowPitchFactor;
+
+        float yawDueToPosition = transform.localPosition.x * positionYawFactor;
+
+        float rollDueToControlThrow = xThrow * controlThrowRollFactor;
+
+        float pitch = pitchDueToPosition + pitchDueToControlThrow;
+        float yaw = yawDueToPosition;
+        float roll = rollDueToControlThrow;
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    private void ProcessPosition()
+    {
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+
+        float clampedXPos = ClampedPos(transform.localPosition.x, xThrow, speed, xRange);
+        float clampedYPos = ClampedPos(transform.localPosition.y, yThrow, speed, yRange);
 
         transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
     }
 
-    private float ClampedPos(float origin, string axis, float speed, float range)
+    private float ClampedPos(float origin, float inputThrow, float speed, float range)
     {
-        float input = CrossPlatformInputManager.GetAxis(axis);
-        float offset = input * speed * Time.deltaTime;
+        float offset = inputThrow * speed * Time.deltaTime;
         float position = origin + offset;
-        
+
         return Mathf.Clamp(position, -range, range);
     }
 
