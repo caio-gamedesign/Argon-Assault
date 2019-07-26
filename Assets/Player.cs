909efefb30cb0bb656cb24e6ffb62dc05b83d1c0
caiogamedesign <caio.gamedesign@gmail.com>
2019-07-26 14:00:58 -0300

    Controlling spaceship position

---
+++ Assets/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+    [Tooltip("In ms^-1")] [SerializeField] float speed = 20f;
+    [Tooltip("In m")] [SerializeField] float xRange = 5f;
+    [Tooltip("In m")] [SerializeField] float yRange = 3f;
+    
+
+    private void Update()
+    {
+        float clampedXPos = ClampedPos(transform.localPosition.x, "Horizontal", speed, xRange);
+        float clampedYPos = ClampedPos(transform.localPosition.y, "Vertical", speed, yRange);
+
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
+    }
+
+    private float ClampedPos(float origin, string axis, float speed, float range)
+    {
+        float input = CrossPlatformInputManager.GetAxis(axis);
+        float offset = input * speed * Time.deltaTime;
+        float position = origin + offset;
+        
+        return Mathf.Clamp(position, -range, range);
+    }
+
+
+
+}
