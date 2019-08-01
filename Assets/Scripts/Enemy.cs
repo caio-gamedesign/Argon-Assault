55f8560233fecf80a1aed99332f8f3a3db8f64bb
caiogamedesign <caio.gamedesign@gmail.com>
2019-08-01 12:37:41 -0300

    Enemies Exploding

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    SphereCollider sphereCollider;
+    bool isAlive = true;
+
+    [SerializeField] GameObject deathFX;
+
+    private void Start()
+    {
+        SetupSphereCollider();
+    }
+
+    private void SetupSphereCollider()
+    {
+        sphereCollider = GetComponent<SphereCollider>();
+        if (sphereCollider == null)
+        {
+            sphereCollider = gameObject.AddComponent(typeof(SphereCollider)) as SphereCollider;
+        }
+
+        sphereCollider.isTrigger = false;
+        sphereCollider.radius = 0.9f;
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        if (isAlive)
+        {
+            isAlive = false;
+            Instantiate(deathFX, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+}
